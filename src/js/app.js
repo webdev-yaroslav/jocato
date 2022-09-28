755e4c25146db83c4cf56682f522e77db5544a81
Ярослав Пономарёв <yaroslavponomarev.pro@gmail.com>
2022-09-28 19:36:05 +0400

    up

--- src/js/app.js
+++ src/js/app.js
@@ -6,3 +6,18 @@ import Swiper, { Navigation, Pagination } from 'swiper';
 
 const swiper = new Swiper()
 
+const hamb = document.querySelector("#hamb");
+const popup = document.querySelector("#popup");
+const menu = document.querySelector("#menu").cloneNode(1);
+
+hamb.addEventListener("click", hambHandler);
+
+function hambHandler(e) {
+    e.preventDefault();
+    popup.classList.toggle("open");
+    renderPopup();
+}
+
+function renderPopup() {
+    popup.appendChild(menu);
+}
