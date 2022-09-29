23a3938f3d5ff3db4735fd8f3ddde04e2e5f8d82
Ярослав Пономарёв <yaroslavponomarev.pro@gmail.com>
2022-09-29 18:45:38 +0400

    popup

--- src/js/app.js
+++ src/js/app.js
@@ -9,12 +9,15 @@ const swiper = new Swiper()
 const hamb = document.querySelector("#hamb");
 const popup = document.querySelector("#popup");
 const menu = document.querySelector("#menu").cloneNode(1);
+const body = document.body;
 
 hamb.addEventListener("click", hambHandler);
 
 function hambHandler(e) {
     e.preventDefault();
     popup.classList.toggle("open");
+    hamb.classList.toggle("active");
+    body.classList.toggle("noscroll");
     renderPopup();
 }
 
